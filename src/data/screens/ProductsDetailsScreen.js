4aa42a352bb0821ca75e49c5b3f97458974e377e
unknown <smartkelvin66@gmail.com>
2023-09-03 07:26:56 +0100

    update

--- src/data/screens/ProductsDetailsScreen.js
+++ src/data/screens/ProductsDetailsScreen.js
@@ -6,6 +6,7 @@ import {
   useWindowDimensions,
   Text,
   ScrollView,
+  Pressable,
 } from "react-native";
 import products from "../products";
 
@@ -18,12 +19,12 @@ const ProductDetailsScreen = () => {
       {/* image carusel */}
       <ScrollView>
         <FlatList
-          data={products.Images}
+          data={product.images}
           renderItem={({ item }) => (
             <Image source={{ uri: item }} style={{ width, aspectRatio: 1 }} />
           )}
           horizontal
-          showsHorizontalScrollIndicator={false}
+          showsHorizontalScrollIndicator={true}
           pagingEnabled
         />
 
@@ -33,6 +34,9 @@ const ProductDetailsScreen = () => {
           <Text style={styles.description}>{product.description}</Text>
         </View>
       </ScrollView>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>add to cart</Text>
+      </Pressable>
     </View>
   );
 };
@@ -53,6 +57,21 @@ const styles = StyleSheet.create({
     fontSize: 18,
     lineHeight: 30,
     fontWeight: "300",
+  },
+  button: {
+    backgroundColor: "black",
+    position: "absolute",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
   },
 });
 
