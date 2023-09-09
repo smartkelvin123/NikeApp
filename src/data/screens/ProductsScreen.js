9b573e2b42e6d29ec5c44003e298edd15ca2c752
unknown <smartkelvin66@gmail.com>
2023-09-09 07:09:51 +0100

    api slice

--- src/data/screens/ProductsScreen.js
+++ src/data/screens/ProductsScreen.js
@@ -1,43 +1,64 @@
-// import { useNavigation } from "@react-navigation/native";
-import { View, Image, FlatList, StyleSheet, Pressable } from "react-native";
-import products from "../products";
-import { useDispatch, useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  FlatList,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { productsSlice } from "../../store/productSlice";
+
+import { useGetProductsQuery } from "../store/apiSlice";
 
 const ProductsScreen = ({ navigation }) => {
-  // const navigation = useNavigation()     not destruction
-  const products = useSelector((state) => state.products.products);
+  // const navigation = useNavigation();
+
   const dispatch = useDispatch();
 
-  const addToCart = () => {
-    dispatch(cartSlice.actions.addCartItem({ product }));
-  };
+  const { data, isLoading, error } = useGetProductsQuery();
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Error fetching products: {error.error}</Text>;
+  }
+
+  const products = data.data;
 
   return (
-    <View>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => (
-          <Pressable
-            onPress={() => navigation.navigate("ProductDetails")}
-            style={styles.itemContainer}
-          >
-            <Image source={{ uri: item.image }} style={styles.image} />
-          </Pressable>
-        )}
-        numColumns={2}
-      />
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <Pressable
+          onPress={() => {
+            // update selected product
+            // dispatch(productsSlice.actions.setSelectedProduct(item.id));
+
+            navigation.navigate("Product Details", { id: item._id });
+          }}
+          style={styles.itemContainer}
+        >
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </Pressable>
+      )}
+      numColumns={2}
+    />
   );
 };
 
-export default ProductsScreen;
 const styles = StyleSheet.create({
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
   itemContainer: {
     width: "50%",
     padding: 1,
   },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
 });
+
+export default ProductsScreen;
--- src/data/screens/ShoppingCart.js
+++ src/data/screens/ShoppingCart.js
@@ -1,49 +1,144 @@
-import { View, Text, FlatList, Pressable, StyleSheet } from "react-native";
-import React from "react";
-import cart from "../../data/cart";
-import CartListItem from "../../component/CartListItem";
+import {
+  Text,
+  FlatList,
+  View,
+  StyleSheet,
+  Pressable,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
+import CartListItem from "../components/CartListItem";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectDeliveryPrice,
+  selectSubtotal,
+  selectTotal,
+  cartSlice,
+} from "../store/cartSlice";
+import {
+  useCreateOrderMutation,
+  useCreatePaymentIntentMutation,
+} from "../store/apiSlice";
+import { useStripe } from "@stripe/stripe-react-native";
 
 const ShoppingCartTotals = () => {
-  <View style={styles.totalContainer}>
-    <View style={styles.row}>
-      <Text style={styles.text}>Subtotal</Text>
-      <Text style={styles.text}>$320.0 US$$</Text>
-    </View>
-    <View style={styles.row}>
-      <Text style={styles.text}>delivery</Text>
-      <Text style={styles.text}>10,000 US$$</Text>
-    </View>
-    <View style={styles.row}>
-      <Text style={styles.textBold}>Total</Text>
-      <Text> style={styles.textBold}$620.0 US$$</Text>
-    </View>
-  </View>;
-};
+  const subtotal = useSelector(selectSubtotal);
+  const deliveryFee = useSelector(selectDeliveryPrice);
+  const total = useSelector(selectTotal);
 
-const ShoppingCart = () => {
   return (
-    <View>
-      <FlatList
-        data={cart}
-        renderItem={({ item }) => <CartListItem cartItem={item} />}
-        ListFooterComponent={ShoppingCartTotals}
-      />
-      <Pressable style={styles.button}>
-        <Text style={styles.buttonText}>Checkout </Text>
-      </Pressable>
+    <View style={styles.totalsContainer}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Subtotal</Text>
+        <Text style={styles.text}>{subtotal} US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Delivery</Text>
+        <Text style={styles.text}>{deliveryFee} US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.textBold}>Total</Text>
+        <Text style={styles.textBold}>{total} US$</Text>
+      </View>
     </View>
   );
 };
 
-export default ShoppingCart;
+const ShoppingCart = () => {
+  const subtotal = useSelector(selectSubtotal);
+  const deliveryFee = useSelector(selectDeliveryPrice);
+  const total = useSelector(selectTotal);
+  const dispatch = useDispatch();
+
+  const cartItems = useSelector((state) => state.cart.items);
+
+  const [createOrder, { data, error, isLoading }] = useCreateOrderMutation();
+
+  const [createPaymentIntent] = useCreatePaymentIntentMutation();
+
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+
+  const onCheckout = async () => {
+    // 1. Create a payment intent
+    const response = await createPaymentIntent({
+      amount: Math.floor(total * 100),
+    });
+    if (response.error) {
+      Alert.alert("Something went wrong");
+      return;
+    }
+
+    // 2. Initialize the Payment sheet
+    const initResponse = await initPaymentSheet({
+      merchantDisplayName: "notJust.dev",
+      paymentIntentClientSecret: response.data.paymentIntent,
+    });
+    if (initResponse.error) {
+      console.log(initResponse.error);
+      Alert.alert("Something went wrong");
+      return;
+    }
+
+    // 3. Present the Payment Sheet from Stripe
+    const paymentResponse = await presentPaymentSheet();
+
+    if (paymentResponse.error) {
+      Alert.alert(
+        `Error code: ${paymentResponse.error.code}`,
+        paymentResponse.error.message
+      );
+      return;
+    }
+
+    // 4. If payment ok -> create the order
+    onCreateOrder();
+  };
+
+  const onCreateOrder = async () => {
+    const result = await createOrder({
+      items: cartItems,
+      subtotal,
+      deliveryFee,
+      total,
+      customer: {
+        name: "Vadim",
+        address: "My home",
+        email: "vadim@notjust.dev",
+      },
+    });
+
+    if (result.data?.status === "OK") {
+      Alert.alert(
+        "Order has been submitted",
+        `Your order reference is: ${result.data.data.ref}`
+      );
+      dispatch(cartSlice.actions.clear());
+    }
+  };
+
+  return (
+    <>
+      <FlatList
+        data={cartItems}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={ShoppingCartTotals}
+      />
+      <Pressable onPress={onCheckout} style={styles.button}>
+        <Text style={styles.buttonText}>
+          Checkout
+          {isLoading && <ActivityIndicator />}
+        </Text>
+      </Pressable>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-  totalContainer: {
-    padding: 20,
-    paddingHorizontal: 20,
-    flexDirection: "row",
+  totalsContainer: {
     margin: 20,
-    borderColor: "white",
+    paddingTop: 10,
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
   },
   row: {
     flexDirection: "row",
@@ -51,25 +146,23 @@ const styles = StyleSheet.create({
     marginVertical: 2,
   },
   text: {
-    color: "white",
-    fontWeight: "bold",
     fontSize: 16,
+    color: "gray",
   },
   textBold: {
-    color: "white",
-    fontWeight: "500",
     fontSize: 16,
-    fontFamily: "500",
+    fontWeight: "500",
   },
+
   button: {
-    backgroundColor: "black",
     position: "absolute",
+    backgroundColor: "black",
     bottom: 30,
     width: "90%",
     alignSelf: "center",
-    alignItems: "center",
     padding: 20,
     borderRadius: 100,
+    alignItems: "center",
   },
   buttonText: {
     color: "white",
@@ -77,3 +170,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
+
+export default ShoppingCart;
